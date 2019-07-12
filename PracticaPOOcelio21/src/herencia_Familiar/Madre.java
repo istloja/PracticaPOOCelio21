212cf9ba6e5b8849dfb2b28a93e13310b1d9fc13
ISTLOJA V <ISTLOJA V@ISTLOJA14>
2019-07-12 10:16:31 -0500

    arbol familiar

---
+++ PracticaPOOcelio21/src/herencia_Familiar/Madre.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herencia_Familiar;
+
+/**
+ *
+ * @author ITS LOJA
+ */
+public class Madre extends Persona {
+   Padre esposo;
+   Abuelo suegro;
+   Abuela suegra;
+   Hijo hijo;
+   Hija hija;
+
+    public Padre getEsposo() {
+        return esposo;
+    }
+
+    public void setEsposo(Padre esposo) {
+        this.esposo = esposo;
+    }
+
+    public Abuelo getSuegro() {
+        return suegro;
+    }
+
+    public void setSuegro(Abuelo suegro) {
+        this.suegro = suegro;
+    }
+
+    public Abuela getSuegra() {
+        return suegra;
+    }
+
+    public void setSuegra(Abuela suegra) {
+        this.suegra = suegra;
+    }
+
+    public Hijo getHijo() {
+        return hijo;
+    }
+
+    public void setHijo(Hijo hijo) {
+        this.hijo = hijo;
+    }
+
+    public Hija getHija() {
+        return hija;
+    }
+
+    public void setHija(Hija hija) {
+        this.hija = hija;
+    }
+   public Madre(){
+    setNombres("Laura");
+    setApellidos("Delgado Quezada");
+    setEdad(47);
+    
+}
+}
